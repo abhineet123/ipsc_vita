c2d469e5fc3914eb56e136913697bceda0dad421
abhineet123 <asingh1@ualberta.ca>
2022-12-11 06:28:31 +0000

    minor fix

--- cmd/vita-ipsc-ext_reorg_roi_g2_0_38_ytvis_swin.sh
+++ cmd/vita-ipsc-ext_reorg_roi_g2_0_38_ytvis_swin.sh
@@ -8,7 +8,7 @@
 #SBATCH --job-name=vita-ipsc-ext_reorg_roi_g2_0_38-swin
 #SBATCH --output=%x_%j.out
 
-#SBATCH --time=0-16:00            # time (DD-HH:MM)
+#SBATCH --time=0-24:00            # time (DD-HH:MM)
 
 #SBATCH --mail-user=asingh1@ualberta.ca
 #SBATCH --mail-type=ALL
@@ -19,7 +19,7 @@ source ~/venv/vita/bin/activate
 
 nvidia-smi
 
-python train_net_vita.py --num-gpus 2 --config-file configs/youtubevis_2019/vita-ipsc-ext_reorg_roi_g2_0_38-vita_SWIN_bs8.yaml MODEL.WEIGHTS pretrained/vita_swin_coco.pth SOLVER.IMS_PER_BATCH 2
+python train_net_vita.py --num-gpus 2 --config-file configs/youtubevis_2019/vita-ipsc-ext_reorg_roi_g2_0_38-vita_SWIN_bs8.yaml MODEL.WEIGHTS pretrained/vita_swin_coco.pth SOLVER.IMS_PER_BATCH 2 --resume
 
 
 
